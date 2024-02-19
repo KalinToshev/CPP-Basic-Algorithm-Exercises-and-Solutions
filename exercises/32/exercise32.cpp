aff0c433c7b5fb58aea786eeec9daca09c0823dc
Kalin Toshev <kalin.toshev84@gmail.com>
2024-02-19 17:29:53 +0200

    Add exercise 32

---
+++ exercises/32/exercise32.cpp
@@ -0,0 +1,19 @@
+/*
+Write a C++ program to compare two given strings and return the number of positions where they contain the same length 2 substring.
+Sample Input:
+{ 5, 5, 1, 15, 15 }
+{ 15, 2, 3, 4, 15 }
+{ 3, 3, 15, 15, 5, 5}
+{ 1, 5, 15, 7, 8, 15}
+Sample Output:
+1
+0
+1
+0
+*/
+
+/*
+============================================
+WRONG DESCRIPTION, SOOO NO SOLUTION THERE
+============================================
+*/
